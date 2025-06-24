5ec3a0d46a4d8d12be95ae5f8780320713deda50
Alex <alexiusluzes@gmail.com>
2025-06-24 16:12:45 -0300

    Update agentes.js

--- src/db/seeds/agentes.js
+++ src/db/seeds/agentes.js
@@ -3,7 +3,7 @@ exports.seed = async function(knex) {
 
   await knex('agentes').insert([
     { nome: 'Grace Grazi', idade: '25', classe: 'Ocultista', trilha: 'Graduado', origem: 'Universitária', estado: 'VIVO' },
-    { nome: 'Gabriela Grazi', idade: '25', classe: 'Combatente', trilha: 'Tropa de Choque', origem: 'Atleta', estado: 'VIVO' },
+    { nome: 'Gabriela Grazi', idade: '25', classe: 'Combatente', trilha: 'Tropa de Choque', origem: 'Atleta', estado: 'MORTO' },
     { nome: 'Yuri Bularque', idade: '28', classe: 'Combatente', trilha: 'Aniquilador', origem: 'Experimento', estado: 'VIVO' }
   ]);
 };
